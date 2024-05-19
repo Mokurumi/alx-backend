16e273211b7f1a860b8e585681680839d9e3b7b0
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-05-19 22:31:49 +0300

    Simple pagination helper

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Simple pagination helper
+'''
+
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    return a tuple of size two containing a start index and an end index
+    '''
+    return ((page - 1) * page_size, page * page_size)
