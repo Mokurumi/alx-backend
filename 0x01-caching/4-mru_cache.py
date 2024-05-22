ee02bb88bbf47bd929cbe54ba039275b2d580762
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-05-23 00:37:36 +0300

    LRU and MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+MRU Caching (Most Recently Used)
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    LRUCache class
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """
+        Put an item in the cache
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data:
+                discard = self.keys.pop()
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+        self.keys.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.keys.remove(key)
+        self.keys.append(key)
+        return self.cache_data[key]
