8343644555f111da58abebed6414ff99d393679e
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-05-23 00:25:03 +0300

    FIFO and LIFO Cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Caching FIFO
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache class
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """
+        Put an item in the cache
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data:
+                discard = self.keys.pop(0)
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+        self.keys.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
