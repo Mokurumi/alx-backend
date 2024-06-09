8514fa22e7cd7214ab80d11603b75738765b645d
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-06-09 17:05:46 +0300

    translations

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config:
+class Config(object):
     """Config"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -28,7 +28,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route("/", methods=["GET"], strict_slashes=False)
 def index():
     """index"""
     g.locale = get_locale()
@@ -36,3 +36,7 @@ def index():
     home_header = gettext("home_header")
     return render_template("3-index.html", home_title=home_title,
                            home_header=home_header, get_locale=get_locale)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="5000")
