3ab8edd613da6fab11de703cf7655384ed07d13c
Alpha Mokurumi <alpha@nathandigital.com>
2024-06-10 17:00:24 +0300

    last 3 try

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -33,10 +33,7 @@ def index():
     """
     hello world
     """
-    home_title = gettext("home_title")
-    home_header = gettext("home_header")
-    return render_template("3-index.html", home_title=home_title,
-                           home_header=home_header, get_locale=get_locale)
+    return render_template("3-index.html", get_locale=get_locale)
 
 
 if __name__ == '__main__':
